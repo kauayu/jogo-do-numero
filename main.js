4dd3f7d3e3cdd6ee1082ba309577a1609102e69e
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-08-14 07:44:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+let numeroAleatorio = Math.floor(Math.random() *100) +1;
+const palpites = document.querySelector('.palpites');
+const ultimoResultado = document.querySelector('.ultimoResultado')
+const baixoOuAlto = document.querySelector
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} else if (contagmePalpites ===10) {
+  ultimoResultado.textContent = ...
+
+
+
+    ultimoResultado.textContent = "Errado";
+  ultimoResultado.style.backgroudolor = "red";
+  if (palpiteUsuario < numeroAleatorio) {
+    baixoOuAlto.textContent = "O ultimo palpite foi muito baixo";
+  }
